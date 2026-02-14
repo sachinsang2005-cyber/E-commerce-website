e5ab2a3b71144c668c8618606232e67d67e6dbfc
Abhishek <sachinsang2005@gmail.com>
2026-02-14 23:04:49 +0530

    Add sampleFarsan data to init/data.js

---
+++ init/data.js
@@ -0,0 +1,26 @@
+const sampleFarsan = [
+  {
+    title: "Tikha Sev",
+    description: "Traditional spicy tikha sev with bold masala flavor, best enjoyed with tea.",
+    image: "https://imgs.search.brave.com/UT8nkmV_UcSb2nShaJu4trZEBPoyEaM-Q865NqdaWB0/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly81Lmlt/aW1nLmNvbS9kYXRh/NS9TRUxMRVIvRGVm/YXVsdC8yMDI1LzEv/NDgwNjU1NDM0L1pS/L1pRL0NRLzU5MDgz/OTMvMWtnLWRlZXBh/LWZhcnNhbi10aWto/YS1zZXYtNTAweDUw/MC5qcGc",
+    price: 140,
+    category: "Sev",
+    weight: "500g",
+    stock: 40,
+    location: "Surat",
+    country: "India"
+  },
+  {
+    title: "Ratlami Sev",
+    description: "Famous spicy Ratlami sev with authentic Indian masala taste and crunchy texture.",
+    image: "https://imgs.search.brave.com/iHksd1_Lh_LZyYO82PoLymq-tTdVQnqD8ehPWxW2LN4/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NzFXcGQ5LWRTcEwu/anBn",
+    price: 160,
+    category: "Sev",
+    weight: "500g",
+    stock: 35,
+    location: "Indore",
+    country: "India"
+  }
+];
+
+module.exports = { data: sampleFarsan };
