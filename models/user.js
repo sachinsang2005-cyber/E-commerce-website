2d946b7b81880f666debe29a4f1ebbd90e94619a
Abhishek <sachinsang2005@gmail.com>
2026-02-14 23:31:08 +0530

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+// IMPORTANT FIX 👇
+const passportLocalMongoose =
+    require("passport-local-mongoose").default ||
+    require("passport-local-mongoose");
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    }
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", userSchema);
